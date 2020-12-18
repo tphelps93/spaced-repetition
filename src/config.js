c5c2054a05fd8642034752e2474af4b80a6f3351
Taylor Phelps <tphelps1993@gmail.com>
2020-12-18 14:05:05 -0500

    updated

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT:
+    process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api',
   TOKEN_KEY: 'blogful-client-auth-token',
-}
+};
